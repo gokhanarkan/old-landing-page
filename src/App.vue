4777b9bd2e12d4fceec84d89cd172fe4c1e7ab0b
Gokhan <gokhanarkan@gmail.com>
2020-05-17 18:24:26 +0100

    Toggle button added for dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <toggle-button @change="changeTheme()" :value="true"
+               :labels="{checked: 'Dark Mode', unchecked: 'Light Mode'}" :color="{checked: '#c8c9cd', unchecked: '#233646'}" :width="80" class="toggle-button"/>
     <About second_occupation="Tea Lover"/>
   </div>
 </template>
@@ -11,6 +13,14 @@ export default {
   name: 'App',
   components: {
     About
+  },
+  methods: {
+    changeTheme: function() {
+      document.body.classList.toggle("light-background");
+      document.getElementById("name").classList.toggle("dark-text");
+      document.getElementById("occupation").classList.toggle("dark-text");
+      document.getElementById("body").classList.toggle("dark-text");
+    }
   }
 }
 </script>
@@ -18,6 +28,20 @@ export default {
 <style>
   body {
     background-color: #233646;
+  }
+  .light-background {
+    background-color: #c8c9cd;
+  }
+  .dark-text {
+    color: #233646 !important;
+  }
+  .toggle-button {
+    float: right;
+  }
+  @media only screen and (min-width: 760px) {
+    .toggle-button {
+      float: left;
+    }
   }
   @import './assets/styles/tufte.min.css';
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,20 @@
 <template>
     <article class="article">
 
-        <h1>Gökhan Arkan</h1>
+        <h1 id="name">Gökhan Arkan</h1>
 
-        <h3 class="subtitle"> Software Engineer &amp; {{ second_occupation }} </h3>
+        <h3 class="subtitle" id="occupation"> Software Engineer &amp; {{ second_occupation }} </h3>
 
         <section class="body">
 
-            <p>I am a backend focused software engineer based in London, UK. (born &amp; raised in Istanbul, Turkey)</p>
-            <p>I work in the fintech industry and studying towards my Master's degree at the University of Oxford.</p>
-            <p>My professional interests are; distributed systems, machine learning, cloud computing and human-computer interaction.</p>
-            <p>I like listening and creating music, cycling and photography/videography.
-                In fact, I'll be glad to accompany charity/volunteer work as a photographer.</p>
-            <p>Although I am not actively using social media, please feel free to 
+            <p id="body">I am a backend focused software engineer based in London, UK. (born &amp; raised in Istanbul, Turkey)<br><br>
+            I work in the fintech industry and studying towards my Master's degree at the University of Oxford.<br><br>
+            My professional interests are; distributed systems, machine learning, cloud computing and human-computer interaction.<br><br>
+            I like listening &amp; creating music, cycling and photography/videography.
+                In fact, I'll be glad to accompany charity/volunteer work as a photographer.<br><br>
+            Although I am not actively using social media, please feel free to
                 <a style="text-decoration:underline;background:unset;text-shadow:unset;" href="mailto:hi@gokhanarkan.com">
-                    email me</a> if you have any enquiries.</p>
+                    email me</a> if you have any enquiries.<br><br></p>
 
         </section>
 
@@ -33,7 +33,11 @@ export default {
 
 
 <style scoped>
+    :root {
+    --main-color: #233646;
+    --light-color: #c8c9cd;
+    }
     .article, h1, p {
-        color:#c8c9cd;;
+        color: #c8c9cd;
     }
 </style>
