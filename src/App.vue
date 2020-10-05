4d1035a12a4d6ffa71d0c8bc3947ac2dbc56a0fe
Gokhan <gokhanarkan@gmail.com>
2020-10-06 01:03:11 +0300

    Updated components along with post request

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="app">
     <v-divider></v-divider>
-    <toggle-button @change="changeTheme()" :value="true"
-               :labels="{checked: 'Dark Mode', unchecked: 'Light Mode'}" :color="{checked: '#6e6e6e', unchecked: '#233646'}" :width="80" class="toggle-button"/>
-    <About second_occupation="Tea Lover"/>
-      <Contact />
-      <div class="handout"></div>
+    <toggle-button
+      @change="changeTheme()"
+      :value="true"
+      :labels="{ checked: 'Dark Mode', unchecked: 'Light Mode' }"
+      :color="{ checked: '#6e6e6e', unchecked: '#233646' }"
+      :width="80"
+      class="toggle-button"
+    />
+    <About second_occupation="Tea Lover" />
+    <Contact />
+    <div class="handout"></div>
   </div>
 </template>
 
 <script>
-import About from './components/About.vue'
-import Contact from './components/Contact'
+  import About from "./components/About.vue";
+  import Contact from "./components/Contact";
 
-export default {
-  name: 'App',
-  components: {
-    About,
-    Contact
-  },
-  methods: {
-    changeTheme: function() {
-      document.body.classList.toggle("light-background");
-      document.getElementById("name").classList.toggle("dark-text");
-      document.getElementById("occupation").classList.toggle("dark-text");
-      document.getElementById("body").classList.toggle("dark-text");
-      document.getElementById("body-other").classList.toggle("dark-text");
-      document.getElementById("projects").classList.toggle("dark-text");
-    }
-  }
-}
+  export default {
+    name: "App",
+    components: {
+      About,
+      Contact,
+    },
+    methods: {
+      changeTheme: function() {
+        document.body.classList.toggle("light-background");
+        document.getElementById("name").classList.toggle("dark-text");
+        document.getElementById("occupation").classList.toggle("dark-text");
+        document.getElementById("body").classList.toggle("dark-text");
+        document.getElementById("body-other").classList.toggle("dark-text");
+        document.getElementById("projects").classList.toggle("dark-text");
+      },
+    },
+  };
 </script>
 
 <style>
@@ -37,7 +43,7 @@ export default {
     background-color: #233646;
   }
   .light-background {
-    background-color: #c8c9cd;
+    background-color: #ffffff;
   }
   .dark-text {
     color: #233646 !important;
@@ -48,5 +54,5 @@ export default {
   .handout {
     padding-bottom: 5rem;
   }
-  @import './assets/styles/tufte.min.css';
+  @import "./assets/styles/tufte.min.css";
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,72 +1,95 @@
 <template>
-    <article class="article">
+  <article class="article">
+    <a
+      class="links"
+      href="https://arkan.me"
+      id="projects"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Projects Blog
+    </a>
+    <br /><br />
 
-        <a class="links" href="https://arkan.me" id="projects" target="_blank"
-            rel="noreferrer noopener">
-            Projects Blog
-        </a>
-        <br><br>
+    <h1 id="name">Gökhan Arkan</h1>
 
-        <h1 id="name">Gökhan Arkan</h1>
+    <h3 class="subtitle" id="occupation">
+      Software Engineer &amp; {{ second_occupation }}
+    </h3>
 
-        <h3 class="subtitle" id="occupation"> Software Engineer &amp; {{ second_occupation }} </h3>
+    <section class="body">
+      <p id="body">
+        I am a backend focused software engineer based in London, UK. (born
+        &amp; raised in Istanbul, Turkey)<br /><br />
+        I work in fintech and studying towards my Master's degree at the
+        University of Oxford.<br /><br />
+        My professional interests are; distributed systems, machine learning,
+        reliability &amp; scalability and human-computer interaction.
+      </p>
 
-        <section class="body">
-
-            <p id="body">I am a backend focused software engineer based in London, UK. (born &amp; raised in Istanbul,
-                Turkey)<br><br>
-                I work in fintech and studying towards my Master's degree at the University of
-                Oxford.<br><br>
-                My professional interests are; distributed systems, machine learning, reliability &amp; scalability and
-                human-computer interaction.</p>
-
-            <p id="body-other">
-                I like listening<label class="margin-toggle sidenote-number" for="band"></label><input type="checkbox"
-                    id="band" class="margin-toggle" /> &amp; creating music,
-                <span class="sidenote"><em>My favourite band is <a class="links" href="https://americanmary.com/"
-                            target="_blank" rel="noreferrer noopener">The National.</a></em></span> cycling and
-                photography/videography.
-                In fact, I'll be glad to accompany charity/volunteer work as a photographer.<br><br>
-                Although I am not actively using social media, please feel free to email me at
-                <a class="links" href="mailto:gokhan@hey.com">
-                    gokhan@hey.com</a> if you have any enquiries.<br><br>
-                Alternatively, click below to contact me.
-            </p>
-
-        </section>
-
-    </article>
+      <p id="body-other">
+        I like listening<label
+          class="margin-toggle sidenote-number"
+          for="band"
+        ></label
+        ><input type="checkbox" id="band" class="margin-toggle" /> &amp;
+        creating music,
+        <span class="sidenote"
+          ><em
+            >My favourite band is
+            <a
+              class="links"
+              href="https://americanmary.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+              >The National.</a
+            ></em
+          ></span
+        >
+        cycling and photography. In fact, I'll be glad to accompany as a
+        photographer for charity.<br /><br />
+        Although I am not actively using social media, please feel free to email
+        me at
+        <a class="links" href="mailto:gokhan@hey.com"> gokhan@hey.com</a> if you
+        have any enquiries.<br /><br />
+        <a class="links" href="https://post.arkan.me" target="_blank"
+          >If you are unreasonable enough, you can send me a POST request,
+          too.</a
+        >
+        <br /><br />
+        Alternatively, use the below button to contact me.
+      </p>
+    </section>
+  </article>
 </template>
 
-
 <script>
-    export default {
-        name: 'About',
-        props: {
-            second_occupation: String
-        }
-    }
+  export default {
+    name: "About",
+    props: {
+      second_occupation: String,
+    },
+  };
 </script>
 
-
 <style scoped>
-    .article,
-    h1,
-    p {
-        color: #c8c9cd;
-    }
+  .article,
+  h1,
+  p {
+    color: #c8c9cd;
+  }
 
-    .article {
-        padding: 0;
-    }
+  .article {
+    padding: 0;
+  }
 
-    .links {
-        text-decoration: underline;
-        background: unset;
-        text-shadow: unset;
-    }
+  .links {
+    text-decoration: underline;
+    background: unset;
+    text-shadow: unset;
+  }
 
-    #projects {
-        float: left;
-    }
+  #projects {
+    float: left;
+  }
 </style>
